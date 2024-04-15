bb9bbc2d757b52a75b0383e0e13954bbc699463e
TZippilli <tzippilli@udesa.edu.ar>
2024-04-15 18:23:32 -0300

    mis productos

--- TI-Prog2/controllers/usersController.js
+++ TI-Prog2/controllers/usersController.js
@@ -1,3 +1,5 @@
+const db = require('../db/data');
+
 const usersController = {
     login: function(req, res, next) {
         res.render('login');
@@ -8,13 +10,12 @@ const usersController = {
     },
 
     profile: function(req, res, next) {
-        res.render("profile");
+        res.render("profile", { db: db }); 
     },
 
     register: function(req, res, next) {
         res.render("register");
     }
-
 }
 
-module.exports = usersController
+module.exports = usersController;
