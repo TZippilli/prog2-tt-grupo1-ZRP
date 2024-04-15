d61d8250954a5e2d471933083e70ee89acd7ee51
TZippilli <tzippilli@udesa.edu.ar>
2024-04-15 18:05:30 -0300

    aparecen las fotos de los autos

--- TI-Prog2/controllers/indexController.js
+++ TI-Prog2/controllers/indexController.js
@@ -1,12 +1,13 @@
+const db = require('../db/data');
+
 const indexController = {
     index: function(req, res, next) {
-        res.render('index');
+        res.render('index', { db: db }); 
     },
 
     search: function(req, res) {
-        res.render("search-results");
+        res.render("search-results", { db: db });
     }
-
 }
 
-module.exports = indexController
+module.exports = indexController;
--- TI-Prog2/controllers/productController.js
+++ TI-Prog2/controllers/productController.js
@@ -1,12 +1,17 @@
+const db = require('../db/data');
+
 const productController = {
     index: function(req, res, next) {
-        res.render('product');
+        const productId = req.params.id; 
+
+        const producto = db.productos.find(producto => producto.id === productId);
+
+        res.render('product', { producto: producto });
     },
 
     newProd: function(req, res) {
         res.render("product-add");
     }
-
 }
 
-module.exports = productController
+module.exports = productController;
